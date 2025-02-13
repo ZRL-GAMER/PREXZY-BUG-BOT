e23aba237e894d29b85adb2f71cf88bda652e6a4
ZRL-GAMER <zrlgamer999@gmail.com>
2025-02-13 13:45:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '2347037153696'  //creator number
+global.ownername = 'ZRLGAMER' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
